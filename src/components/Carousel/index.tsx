2edfc8e7ea258edb5703525135e4b012248d8f4e
webmiraclepro <webmiraclepro@gmail.com>
2022-11-18 02:43:26 -0800

    fix: carousel renderindicator customize

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,37 +1,61 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 
+
 const banner = "/images/banner.png"
+const banner1 = "/images/banner1.png"
 
- const NextJsCarousel = () => {
-	
-		return (
-			<div className="">
-			<Carousel
-            showThumbs={false}
-            emulateTouch={true}
-            showArrows={false}
-            infiniteLoop={true}
-            transitionTime={1000}
+const NextJsCarousel = () => {
+
+    return (
+        <div className="">
+            <Carousel
+                showThumbs={false}
+                emulateTouch={true}
+                showArrows={false}
+                infiniteLoop={true}
+                interval={3000}
+                autoPlay={true}
+                renderIndicator={
+                    (clickHandler: (e: React.MouseEvent | React.KeyboardEvent) => void,
+                        isSelected: boolean,
+                        index: number,
+                        label: string) => {
+
+                        if (isSelected) {
+                            return (
+                                <li className="bg-[#475569] rounded-xl w-[36px] h-[12px] cursor-pointer inline-flex mx-2" />
+                            );
+                        }
+                        return (
+                            <li
+                                onClick={clickHandler}
+                                className="bg-[#94A3B8] rounded-xl w-[12px] h-[12px] inline-flex mx-2 cursor-pointer"
+                                value={index}
+                                key={index}
+                                role="button"
+                                tabIndex={0}
+                                
+                            />
+                        );
+                    }
+                }
             >
-				<div>
-					<img src={banner} alt="image1"/>
-				</div>
-				<div>
-					<img src={banner} alt="image2" />
-
-				</div>
-				<div>
-					<img src={banner} alt="image3"/>
-
-				</div>
-				<div>
-					<img src={banner} alt="image4"/>
-
-				</div>
-			</Carousel>
-			</div>
-		);
+                <div>
+                    <img src={banner1} alt="image1" />
+                </div>
+                <div>
+                    <img src={banner1} alt="image2" />
+                </div>
+                <div>
+                    <img src={banner1} alt="image3" />
+                </div>
+                <div>
+                    <img src={banner1} alt="image4" />
+                </div>
+            </Carousel>
+        </div>
+    );
 };
 
 export default NextJsCarousel;
